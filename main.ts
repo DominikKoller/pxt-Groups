303eed9e1aa2097f9d24b08b577da3d250ff584a
Dominik Koller <DominikKoller@users.noreply.github.com>
2019-03-01 16:35:29 +0000

    Very first outlines to use in brief

--- main.ts
+++ main.ts
@@ -5,3 +5,14 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `);
+
+let me: number;
+let message: string;
+
+function join_Party(name: string) {
+
+}
+
+function random_Party_Member(): number {
+    return 0;
+}
