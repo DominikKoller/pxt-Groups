1d042e66038c6a0d1e2c5586bf3be10bf211d0e1
Dominik Koller <DominikKoller@users.noreply.github.com>
2019-03-01 16:44:42 +0000

    Importing Blocks correctly

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
-let me: number;
-let message: string;
+/**
+ * Provides access to communication functionality.
+ */
+//% color=90 weight=100
 
-function join_Party(name: string) {
+namespace parties {
+    let me: number;
+    let message: string;
 
-}
+    //% block
+    export function join_Party(name: string) {
 
-function random_Party_Member(): number {
-    return 0;
+    }
+
+    //% block
+    export function random_Party_Member(): number {
+        return 0;
+    }
 }
