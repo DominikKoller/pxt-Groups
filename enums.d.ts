5253b3499e9530c7e88f4d64e43532b9c82b3fd0
OzYossarian <alex.townsend-teague@outlook.com>
2019-04-21 18:22:49 +0100

    Initial commit after pxt install

--- enums.d.ts
+++ enums.d.ts
@@ -4,12 +4,14 @@
     declare const enum PacketType {
     HEARTBEAT = 7,
     UNICAST_STRING = 8,
+    UNICAST_NUMBER = 9,
     }
 
 
     declare const enum PayloadType {
     NONE = 0,
     STRING = 1,
+    NUM = 2,
     }
 
 // Auto-generated. Do not edit. Really.
