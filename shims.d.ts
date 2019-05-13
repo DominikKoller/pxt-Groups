9ca925a662e3351cf11fe3484068ea63e30c021f
AlexMorson <alexmorson@btinternet.com>
2019-05-13 20:19:18 +0100

    Update shims

--- shims.d.ts
+++ shims.d.ts
@@ -1,48 +1,52 @@
 // Auto-generated. Do not edit.
-declare namespace PartiesInternal {
+declare namespace parties {
+
+    /**
+     * Set your own status, for others to see.
+     */
+    //% weight=60
+    //% blockId=set_status block="Set my status to %s" shim=parties::setStatus
+    function setStatus(s: number): void;
+
+    /**
+     * Your own current status.
+     */
+    //% weight=60
+    //% blockId=get_status block="my status" shim=parties::getStatus
+    function getStatus(): int32;
 
     /**
      * Filters out old entries in the table
      * Also call at HEARTBEAT_FREQUENCY from TS
      */
-    //% shim=PartiesInternal::filterTable
+    //% shim=parties::filterTable
     function filterTable(): void;
 
     /**
      * Read a packet from the queue of received packets and react accordingly
      */
-    //% shim=PartiesInternal::receiveData
+    //% shim=parties::receiveData
     function receiveData(): void;
 
     /** 
      * To be called at Heartbeat Frequency
      */
-    //% shim=PartiesInternal::sendHeartbeat
+    //% shim=parties::sendHeartbeat
     function sendHeartbeat(): void;
-
-    /**
-     * Send a string to all micro:bits in the party.
-     */
-    //% shim=PartiesInternal::broadcastString
-    function broadcastString(message: string): void;
-
-    /**
-     * Send a string to the micro:bit with the specified address
-     */
-    //% shim=PartiesInternal::unicastString
-    function unicastString(message: string, destAddress: uint32): void;
 
     /**
      * Send a number to all micro:bits in the party.
      */
-    //% shim=PartiesInternal::broadcastNumber
-    function broadcastNumber(number: int32): void;
+    //% weight=60
+    //% blockId=party_broadcast_number block="Send %value to all party members" shim=parties::broadcastNumber
+    function broadcastNumber(number: number): void;
 
     /**
      * Send a number to the micro:bit with the specified address
+     * duplicate to TS because we cannot use PartyMember type as address.
      */
-    //% shim=PartiesInternal::unicastNumber
-    function unicastNumber(number: int32, destAddress: uint32): void;
+    //% shim=parties::unicastNumberAddress
+    function unicastNumberAddress(number: number, destAddress: number): void;
 
     /**
      * Use this only to call receiveData from Typescript
@@ -50,43 +54,52 @@ declare namespace PartiesInternal {
      * Note: Only one function can be registered at once, so the radio module
      * will have to be disabled.
      */
-    //% shim=PartiesInternal::onDataReceived
+    //% shim=parties::onDataReceived
     function onDataReceived(body: () => void): void;
 
     /**
      * Use as frequency to call sendHeartbeat
      */
-    //% shim=PartiesInternal::getHeartbeatFrequency
+    //% shim=parties::getHeartbeatFrequency
     function getHeartbeatFrequency(): int32;
 
     /**
      * Numer of Party Members
      */
-    //% shim=PartiesInternal::numberOfPartyMembers
-    function numberOfPartyMembers(): int32;
+    //% weight=40
+    //% blockId=party_size block="party size" shim=parties::partySize
+    function partySize(): int32;
 
     /**
-     * Random Party Member
+     * Get the address of the party member at the given index
+     * This is a workaround as we can neither pass lists nor structs to TS
      */
-    //% shim=PartiesInternal::randomPartyMember
-    function randomPartyMember(): uint32;
+    //% shim=parties::addressOfPartyMember
+    function addressOfPartyMember(i: number): uint32;
+
+    /**
+     * Get the status of the party member at the given index
+     * This is a workaround as we can neither pass lists nor structs to TS
+     */
+    //% shim=parties::statusOfPartyMember
+    function statusOfPartyMember(i: number): int32;
 
     /**
      * For TS to check whether there is a new payload to react to
      */
-    //% shim=PartiesInternal::receivedPayloadType
+    //% shim=parties::receivedPayloadType
     function receivedPayloadType(): PayloadType;
 
     /**
      * Get the received string
      */
-    //% shim=PartiesInternal::receivedStringPayload
+    //% shim=parties::receivedStringPayload
     function receivedStringPayload(): string;
 
     /**
      * Get the received number
      */
-    //% shim=PartiesInternal::receivedNumberPayload
+    //% shim=parties::receivedNumberPayload
     function receivedNumberPayload(): int32;
 }
 
